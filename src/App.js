f078c18fcbd0bf9479861d34771c57e0207e52fe
mathias-tad <84601874+mathias-tad@users.noreply.github.com>
2025-02-27 16:13:46 +0300

    update

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import telegram from "./telegram-brands.svg";
 import whatsapp from "./whatsapp.svg";
 import envelope from "./envelope.svg";
 import github from "./github-brands.svg";
+import pointer from "./feather-pointed-solid.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <div className="App">
+    <div className="App">
+      <div className="heading">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <NavLink />
@@ -47,6 +48,7 @@ function App() {
           </div>
         </div>
       </div>
+      <About />
       <Contacts />
     </div>
   );
@@ -59,6 +61,90 @@ class NavLink extends React.Component {
         <li>Projects</li>
         <li>Contacts</li>
       </ul>
+    );
+  }
+}
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: "none",
+    };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  clickHandler(e) {
+    e.preventDefault();
+    this.setState({
+      display: "block",
+    });
+  }
+  render() {
+    return (
+      <div className="about">
+        <div className="first">
+          <h2>Get to Know Me</h2>
+          <button className="show-cert" onClick={this.clickHandler}>
+            Explore My Credentials
+          </button>
+          <div className="certificates" style={{ display: this.state.display }}>
+            <a href="https://freecodecamp.org/certification/fcc3c89f9c4-29bb-40e5-b05c-32fb1676224f/responsive-web-design">
+              <p>Responsive Web Design</p>
+            </a>
+            <a href="https://freecodecamp.org/certification/fcc3c89f9c4-29bb-40e5-b05c-32fb1676224f/javascript-algorithms-and-data-structures-v8">
+              <p>JavaScript Algorithms and Data Structures</p>
+            </a>
+          </div>
+        </div>
+        <p className="description">
+          "I’m a web app developer committed to building fast, responsive, and
+          scalable applications. With a strong foundation in front-end and
+          back-end development, I turn ideas into functional, user-centered
+          digital solutions. Passionate about innovation and continuous
+          learning, I strive to create seamless web experiences that stand out."
+        </p>
+        <div className="skills">
+          <h3>Core Skills</h3>
+          <ul>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp;{" "}
+              <span>HTML, CSS, JS and React framework for front-end</span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp;
+              <span>
+                Node.js, Django for back-end and SQL, MongoDB for database
+              </span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp; <span>Git and GitHub version control</span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp; <span>RESTful APIs</span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp; <span>AWS, Netlify and Docker deployment and hosting</span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp; <span>Problem solving</span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp; <span>Team work and agile collaboration</span>
+            </li>
+            <li>
+              <img src={pointer} alt="->" />
+              &nbsp; <span>Basic web security principles</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
